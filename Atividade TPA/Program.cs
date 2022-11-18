143d4d0f8e9aea484120f261d3f04f4e0e8b5494
StarNight <luis1604lf@gmail.com>
2022-11-18 11:50:46 -0300

    Atividade de TPA parte 1

---
+++ Atividade TPA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_TPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string[] nome = new string[5];
+            int[] pontos = new int[5];
+
+
+            for(i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Escreva o {0}° nome", i+1);
+                nome[i] = Console.ReadLine();
+
+            }
+            for (i = 0; i <= 4; i++)
+                {
+                    Console.WriteLine("Escreva os pontos do {0}° time", i + 1);
+                    pontos[i] = int.Parse(Console.ReadLine());
+
+                }
+        }
+    }
+}
